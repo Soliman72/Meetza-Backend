3b16eb42ab56d7b923e5e6f01219d8b3d6ac42b1
Shahd Saed <shahd01278039699@gmail.com>
2026-04-24 04:01:06 +0300

    updates

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,7 +6,7 @@ const { checkAdminPermission } = require("../middleware/checkAdminPermission");
 const uploadMiddleware = require("../middleware/uploadMiddleware");
 
 router.get("/", verifyToken, checkAdminPermission, userController.getAllUsers);
-router.get("/:id", verifyToken, checkAdminPermission, userController.getUserById);
+router.get("/:id", verifyToken, userController.getUserById);
 router.get("/email/:email", verifyToken, checkAdminPermission, userController.getUserByEmail);
 router.patch("/:id", verifyToken, uploadMiddleware, userController.updateUser);
 router.delete("/:id", verifyToken, checkAdminPermission, userController.deleteUser);
