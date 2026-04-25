a97de3203f26de281c93229711b2b10948c92dc1
Soliman72 <solimanelaraby743@gmail.com>
2026-04-25 15:37:27 +0300

    domain seed file

--- seeders/index.js
+++ seeders/index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const seedAdmins = require("./seeds/administratorSeed");
 const seedGroups = require("./seeds/groupSeed");
+const seedDomains = require("./seeds/domainSeed");
 
 async function runSeeds() {
   try {
     await seedAdmins();
     await seedGroups();
+    await seedDomains();
 
     console.log("Seeding Done");
     process.exit();
