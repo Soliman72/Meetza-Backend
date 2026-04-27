6ac433142a716a2c00d63a783b255ebf7f4d2695
Soliman72 <solimanelaraby743@gmail.com>
2026-04-27 19:56:17 +0300

    update swagger doc

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const { Server } = require("socket.io");
 const setupSwagger = require("./src/config/setupSwagger");
 require("./src/config/db");
 
-const http = require("http");
 const fs = require("fs");
 const os = require("os");
-const { Server } = require("socket.io");
 
 const { createApp } = require("./app");
 const chatController = require("./src/controllers/chatController");
