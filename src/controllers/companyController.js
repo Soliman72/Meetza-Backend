581253bf2c1ddf2ae74e7d0f518032811ca65c0e
Shahd Saed <shahd01278039699@gmail.com>
2026-04-29 20:30:06 +0300

    update brnding

--- src/controllers/companyController.js
+++ src/controllers/companyController.js
@@ -1,15 +1,6 @@
 const companyService = require("../services/companyService");
 require("dotenv").config();
 const companyId = process.env.COMPANY_ID;
-const resolveCompanyId = (req) => {
-  const id = req.params?.id == "id" ? companyId : req.params?.id;
-  if (!id) {
-    const err = new Error("Company id is required");
-    err.status = 400;
-    throw err;
-  }
-  return id;
-};
 const sendError = (res, err) => {
   const status =
     err.status && err.status >= 400 && err.status < 600 ? err.status : 500;
@@ -40,7 +31,7 @@ exports.list = async (req, res) => {
 
 exports.getById = async (req, res) => {
   try {
-    const data = await companyService.getCompanyById(resolveCompanyId(req));
+    const data = await companyService.getCompanyById(companyId);
     return res.json({ success: true, data });
   } catch (err) {
     return sendError(res, err);
@@ -49,7 +40,7 @@ exports.getById = async (req, res) => {
 
 exports.update = async (req, res) => {
   try {
-    const data = await companyService.updateCompany(resolveCompanyId(req), req.body);
+    const data = await companyService.updateCompany(companyId, req.body);
     return res.json({ success: true, data });
   } catch (err) {
     return sendError(res, err);
@@ -58,7 +49,7 @@ exports.update = async (req, res) => {
 
 exports.remove = async (req, res) => {
   try {
-    await companyService.deleteCompany(resolveCompanyId(req));
+    await companyService.deleteCompany(companyId);
     return res.json({ success: true, message: "Company deleted" });
   } catch (err) {
     return sendError(res, err);
@@ -67,7 +58,7 @@ exports.remove = async (req, res) => {
 
 exports.patchSettings = async (req, res) => {
   try {
-    const data = await companyService.patchCompanySettings(resolveCompanyId(req), req.body);
+    const data = await companyService.patchCompanySettings(companyId, req.body);
     return res.json({ success: true, data });
   } catch (err) {
     return sendError(res, err);
@@ -77,7 +68,7 @@ exports.patchSettings = async (req, res) => {
 exports.patchLogo = async (req, res) => {
   try {
     const file = req.files?.company_logo?.[0];
-    const data = await companyService.updateCompanyLogo(resolveCompanyId(req), file);
+    const data = await companyService.updateCompanyLogo(companyId, file);
     return res.json({ success: true, data });
   } catch (err) {
     return sendError(res, err);
@@ -86,7 +77,7 @@ exports.patchLogo = async (req, res) => {
 
 exports.addDomain = async (req, res) => {
   try {
-    const data = await companyService.addCompanyDomain(resolveCompanyId(req), req.body);
+    const data = await companyService.addCompanyDomain(companyId, req.body);
     return res.status(201).json({ success: true, data });
   } catch (err) {
     return sendError(res, err);
@@ -96,7 +87,7 @@ exports.addDomain = async (req, res) => {
 exports.updateDomain = async (req, res) => {
   try {
     const data = await companyService.updateCompanyDomain(
-      resolveCompanyId(req),
+      companyId,
       req.params.domainId,
       req.body
     );
@@ -108,7 +99,7 @@ exports.updateDomain = async (req, res) => {
 
 exports.removeDomain = async (req, res) => {
   try {
-    await companyService.removeCompanyDomain(resolveCompanyId(req), req.params.domainId);
+    await companyService.removeCompanyDomain(companyId, req.params.domainId);
     return res.json({ success: true, message: "Domain removed" });
   } catch (err) {
     return sendError(res, err);
